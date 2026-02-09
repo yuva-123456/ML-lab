98e3ee33498237df046e032c7145db0de686bc5a
yuva-123456 <yashu.spam2006@gmail.com>
2026-02-09 12:22:02 +0530

    Update apply.py

--- apply.py
+++ apply.py
@@ -1,25 +1,17 @@
 import pandas as pd
-import matplotlib.pyplot as plt
+import numpy as np
 
-s = pd.Series([12, 25, 18, 30, 22, 28, 35])
+s = pd.Series([10, 15, 20, 25, 30])
 
-def plot_graph(kind, title):
-    plt.figure()
-    if kind == "line":
-        plt.plot(s)
-    elif kind == "bar":
-        plt.bar(s.index, s)
-    elif kind == "scatter":
-        plt.scatter(s.index, s)
-    elif kind == "hist":
-        plt.hist(s)
-    elif kind == "box":
-        plt.boxplot(s)
-    plt.title(title)
-    plt.show()
+def process(x):
+    if x % 2 == 0:
+        return x * 2
+    else:
+        return x + 5
 
-plot_graph("line", "Line Plot")
-plot_graph("bar", "Bar Plot")
-plot_graph("scatter", "Scatter Plot")
-plot_graph("hist", "Histogram")
-plot_graph("box", "Box Plot")
+print(s)
+print(s.apply(process))
+print(s.apply(lambda x: x ** 2))
+print(s.apply(np.sqrt))
+print(s.apply(str))
+print(s.apply(len))
